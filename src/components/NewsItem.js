84c289ad5b54edbec5059e9cca6e7f6fcb2008ed
Asif <mdasifhussain268@gmail.com>
2023-12-15 20:16:54 +0530

    added api call

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,11 +8,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: '18rem'}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={imageUrl != null ? imageUrl : "https://images.news18.com/ibnlive/uploads/2023/12/sabarimala-massive-rush-kerala-cm-vijayan-2023-12-56f6aa6a29fdfe4dd72b711ae188f288-16x9.png?impolicy=website&width=1200&height=675"} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+                <h5 className="card-title">{title != null ? title : ""}...</h5>
+                <p className="card-text">{description != null ? description : ""}...</p>
+                <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
             </div>
         </div>
       </div>
